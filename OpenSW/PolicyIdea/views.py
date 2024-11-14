7ac27080bd23c1e6ab40510ce14cf3771d6a8abc
DESKTOP-HH9K4T9\dbgkw <dbgkwkdgml33@gmail.com>
2024-11-15 02:50:27 +0900

    기능 전부 추가해보고, 수정받기

--- OpenSW/PolicyIdea/views.py
+++ OpenSW/PolicyIdea/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import PolicyIdea, Evaluation, Like, Scrap
+from .serializers import PolicyIdeaSerializer, EvaluationSerializer, LikeSerializer, ScrapSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+# 글쓰기 기능을 위한 뷰셋
+class PolicyIdeaViewSet(viewsets.ModelViewSet):
+    queryset = PolicyIdea.objects.all()
+    serializer_class = PolicyIdeaSerializer
+    permission_classes = [IsAuthenticated]  # 로그인된 사용자만 접근 가능
+
+# 평가 기능을 위한 뷰셋
+class EvaluationViewSet(viewsets.ModelViewSet):
+    queryset = Evaluation.objects.all()
+    serializer_class = EvaluationSerializer
+    permission_classes = [IsAuthenticated]
+
+# 좋아요 기능을 위한 뷰셋
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsAuthenticated]
+
+# 스크랩 기능을 위한 뷰셋
+class ScrapViewSet(viewsets.ModelViewSet):
+    queryset = Scrap.objects.all()
+    serializer_class = ScrapSerializer
+    permission_classes = [IsAuthenticated]
