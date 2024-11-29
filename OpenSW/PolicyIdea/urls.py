c88aa6f452add14cd8bf9dd8bd26b737407013c3
narae18 <starestarstar@naver.com>
2024-11-29 11:33:37 +0900

    feat:쇼키님 코드추가

--- OpenSW/PolicyIdea/urls.py
+++ OpenSW/PolicyIdea/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from rest_framework.routers import DefaultRouter
-from .views import PolicyIdeaViewSet, EvaluationViewSet, LikeViewSet, ScrapViewSet
+from .views import PolicyIdeaViewSet, EvaluationViewSet, LikeViewSet, ScrapViewSet, TagViewSet
 
 # 라우터 생성 및 뷰셋 등록
 router = DefaultRouter()
@@ -11,6 +11,7 @@ router.register(r'policy_ideas', PolicyIdeaViewSet)  # 글쓰기 기능
 router.register(r'evaluations', EvaluationViewSet)   # 평가 기능
 router.register(r'likes', LikeViewSet)               # 좋아요 기능
 router.register(r'scraps', ScrapViewSet)             # 스크랩 기능
+router.register(r'tags', TagViewSet, basename='tag')  # 태그 등록
 
 # URL 패턴 설정
 urlpatterns = [
